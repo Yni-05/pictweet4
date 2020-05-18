2da12bb589486a5dc1ebd0d83dbd999ade0f489e
Yni-05 <yhforwork1@gmail.com>
2020-05-18 21:00:01 +0900

    マイグレーションファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_064431) do
+ActiveRecord::Schema.define(version: 2020_05_18_115855) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
